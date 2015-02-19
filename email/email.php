cc6e10133e4669e177fa81f00e7efcb86376b80b
root <root@faster@jakub-krizka.eu>
2015-02-19 12:59:05 +0100

    edit folders

---
+++ email/email.php
@@ -0,0 +1,97 @@
+<?php 
+// check if isset main information
+// kontrola vyplnění povinný údajů
+if (!empty($_POST["emailfrom"]) && !empty($_POST["emailto"]) && !empty($_POST["emailname"]) && !empty($_POST["emailmassage"])) {
+    
+  // if ok, take information from post request and continue
+  // pokud ok deklarace proměnných z formuláře POST
+  $email_from=$_POST["emailfrom"];
+  $email_to=$_POST["emailto"];
+  $email_name=$_POST["emailname"];
+  $email_massage=$_POST["emailmassage"];
+    
+  // define some aditional information, like time, ip adress, HTTP user agent etc.
+  // definice dodatečných informací, jako datum, čas, ip adresa a HTTP user agent
+  $date = date("d.m.Y");
+  $time = date("H:i:s T e");
+  $user_agent = $_SERVER["HTTP_USER_AGENT"];
+  $ip_address = $_SERVER["REMOTE_ADDR"];
+    
+  // define header to email, show mess like html page
+  // definování stylu a jazyku zprávy
+  $headers .= "MIME-Version: 1.0\r\n";
+  $headers .= "Content-Type: text/html; charset=ISO-8859-1\r\n";
+    
+  // subject
+  // předmět
+  $subject = "PHP Email from $email_name";
+    
+  // define template of message
+  // šablona zrávy emailu
+  $mess = "<html><body>";
+  $mess .= "<h1>Email from: $email_from</h1>";
+  $mess .= "<h1>Email to: $email_to</h1>";
+  $mess .= "<h1>Name: $email_name</h1>";
+  $mess .= "<hr>";
+  $mess .= "<h2>From IP: $ip_address</h2>";
+  $mess .= "<h2>$date ($time)</h2>";
+  $mess .= "<h3>User agent: $user_agent</h3>";
+  $mess .= "<hr>";
+  $mess .= "<hr>";
+  $mess .= "<p>Message: $email_massage</p>";
+  $mess .= '</body></html>';
+    
+  // send email
+  // odeslat email
+  $send = mail ($email_to, $subject, $mess, $headers); 
+    
+  // check if send ok
+  // kontrola odeslání emailu
+  if($send) {
+  ?>
+    <script type="text/javascript">
+    
+      // info alert function
+      // výpis výsledku
+      alert("Email se úspěšně odeslal");
+      
+      // redirect back
+      // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+      window.history.back();
+    </script>
+  <?php
+  }
+  // if send failed
+  // pokud odeslání selhalo
+  else {
+    ?>
+      <script type="text/javascript">
+      
+        // info alert function
+        // výpis výsledku
+        alert("Chyba, email nebyl odeslán!");
+        
+        // redirect back
+        // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+        window.history.back();
+      </script>
+    <?php
+  }
+}
+// if is not define all input fileds
+// pokud nejsou definovány všechna povinná pole
+else {  
+  ?>
+    <script type="text/javascript">
+    
+      // info alert function
+      // výpis výsledku
+      alert("Nejsou vyplněna povinná pole");
+      
+      // redirect back
+      // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+      window.history.back();
+    </script>
+  <?php
+}
+?>
---
+++ login/login.php
@@ -0,0 +1,100 @@
+<?php
+
+// check if isset required POST information
+// kontrola povinných údajů z formuláře POST
+if (!empty($_POST["loginname"]) && !empty($_POST["loginpass"])) {
+  
+  // define POST variabley, we can define niminum conditions or terms
+  // definice proměných, můžeme dále nastavit povinnou délkou nebo zakázat speciální znaky
+  $loginname=$_POST["loginname"];
+  $loginpass=$_POST["loginpass"];
+  
+  // define mysql login information
+  // nastavení přístupu do databáze (adresa, uživatel, heslo)
+  $id_spojeni = mysql_connect("localhost","czjakubkrizka","DD89F17CBF");
+  
+  // select database 
+  // výběr databáze
+  $vysledek_vybrani = mysql_select_db("czjakubkrizka",$id_spojeni);
+  
+  // send request to mysql !!! this is unsecure solution, injection isn't resolved !!!
+  // odeslání požadavku na databázový server, kde se hledá v tabulce "users" uživatel jako "id"
+  // POZOR !!! není vyřešena bezpečnost
+  $select = mysql_query("SELECT * from users where id = '$loginname'");
+  
+  // define result
+  // definování výsledku, přiřazení proměných
+  $result=MySQL_Fetch_Array($select);
+  $pass=$result["pass"];
+  
+  // define number od result
+  // vrátí počet výsledků
+  $control=mysql_num_rows($select);
+  
+  // must be only one user
+  // podmínka, musí být pouze jeden výsledek
+  if ($control == 1) {
+      
+    // check if agrees password
+    // kontrola jestli sedí heslo
+    if ($pass == $loginpass) {
+    ?>
+      <script type="text/javascript">
+        
+        // info alert function
+        // výpis výsledku
+        alert("Přihlášení úspěšné");
+        
+        // redirect back
+        // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+        window.history.back();
+      </script>
+    <?php
+    }
+    
+    // if pass fail
+    // pokud heslo nesouhlasíí
+    else {
+    ?>
+      <script type="text/javascript">
+        
+        // info alert function
+        // výpis výsledku
+        alert("Heslo je chybné");
+        
+        // redirect back
+        // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+        window.history.back();
+      </script>
+    <?php
+    }
+  }
+
+  // if user do not exist or more result
+  // pokud nebyl vrácen jeden výsledek nebo výsledků více
+  else {
+  ?>
+    <script type="text/javascript">
+    
+      // info alert function
+      // výpis výsledku
+      alert("Uživatel neexistuje");
+      
+      // redirect back
+      // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+      window.history.back();
+    </script>
+  <?php  
+  }  
+} else
+  ?>
+    <script type="text/javascript">
+    
+      // info alert function
+      // výpis výsledku
+      alert("Nejsou vyplněny některé pole");
+      
+      // redirect back
+      // přesměrování zpět, nebo kamkoliv jinam nebo třeba zápis do cookies atd...
+      window.history.back();
+    </script>
